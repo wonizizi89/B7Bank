dd51f17ff79b9a6b0e72738235f93b808bf72321
Lee Sol <leeso4567@naver.com>
2022-11-24 14:42:06 +0900

    add getter method

--- src/program/Account.java
+++ src/program/Account.java
@@ -6,11 +6,26 @@ public class Account {
     private int balance; // 일단은 int로 해놨으나 돈을 표현하는데 있어서 Decimal 형이 적절하므로 추후 수정
     private String bankName; // 필요?
 
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
     public Account(String ownerName, String accountNumber, String bankName) {
         this.ownerName = ownerName;
         this.accountNumber = accountNumber;
         this.balance = 0;
         this.bankName = bankName;
     }
-
 }
