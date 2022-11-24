93646418183f9ce1bd5a68d5e6888f210390a6ce
klaXXi <baek940dog@gmail.com>
2022-11-24 21:33:26 +0900

    접근 제어자 수정, 모듈 오류 수정, editMemo 메소드 이름 수정

--- src/program/Account.java
+++ src/program/Account.java
@@ -1,55 +1,72 @@
 package program;
 
-public class Account {                                            //계좌생성에 필요한것 ? 이름,계좌번호,잔액,은행명
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
     private String ownerName;
     private String accountNumber;
-    private int balance; // 일단은 int로 해놨으나 돈을 표현하는데 있어서 Decimal 형이 적절하므로 추후 수정
-    private String bankName; // 필요?
-    
+    private int balance;
+    private String bankName;
+    private List<History> histories;
+
     public Account(String ownerName, String accountNumber, String bankName) {
         this.ownerName = ownerName;
         this.accountNumber = accountNumber;
         this.balance = 0;
-//        this.bankName = bankName;
+        this.bankName = bankName;
+        this.histories = new ArrayList<>();
     }
-        private String getAccountNumber() {
-            return accountNumber;
+
+    protected String getOwnerName() {
+        return this.ownerName;
+    }
+
+    protected String getAccountNumber() {
+        return accountNumber;
+    }
+
+    protected int getBalance() {
+        return balance;
+    }
+
+    protected String getBankName() {
+        return bankName;
+    }
+
+    public void addHistory(TradeType type, BigDecimal amount, BigDecimal balance, String traderName) {
+        History history = new History(OffsetDateTime.now(), this.accountNumber, type, amount, balance, traderName);
+        histories.add(history);
+    }
+
+    public void printHistories() {
+        StringBuilder historyBuilder = new StringBuilder();
+
+        for (int i = 0; i < histories.size(); i++) {
+            History singleHistory = histories.get(i);
+            historyBuilder.append(String.format("%d. %s, %s, %s원 ", i + 1, singleHistory.getTraderName(),
+                    singleHistory.getTypeByString(), singleHistory.getAmount()));
+            historyBuilder.append(String.format("[%s %s]%s", singleHistory.getTransactionDate(),
+                    singleHistory.getTransactionTime(), System.lineSeparator()));
         }
-//        private void setAccountNumber (String accountNumber){
-//            this.accountNumber = accountNumber;
-//        }
-        private String getOwnerName() {
-            return ownerName;
-        }
-//        protected void setOwnerName (String ownerName){
-//            this.ownerName = ownerName;
-//        }
-        private int getBalance() {
-            return balance;
-        }
-//        protected void setBalance (int balance){
-//            this.balance = balance;
-//        }
-        private String getBankName() {
-            return bankName;
-        }
-        public void editMemo(String newOwnerName, String newAccountNumber, String newBankName) {
-            this.ownerName = newOwnerName;
-            this.accountNumber = newAccountNumber;
-            this.bankName = newBankName;
-        }
-        public int withdraw(int amount) throws Exception {
+
+        System.out.println(historyBuilder.toString());
+    }
+
+    public void editAccount(String newOwnerName, String newAccountNumber, String newBankName) {
+        this.ownerName = newOwnerName;
+        this.accountNumber = newAccountNumber;
+        this.bankName = newBankName;
+    }
+
+    public int withdraw(int amount) throws Exception {
         if (this.balance < amount) {
             throw new Exception("잔액이 모자랍니다.");
         } else {
             this.balance -= amount;
             return amount;
         }
-        }
-
-//        protected void setBankName (String bankName){
-//            this.bankName = bankName;
-//        }
-
-
+    }
 }
