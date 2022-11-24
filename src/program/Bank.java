429769a0c379fb0245e10755db90256c75af9b67
Lee Sol <leeso4567@naver.com>
2022-11-24 14:43:05 +0900

    add registerCustomer method, checkAccountByCustomerName method

--- src/program/Bank.java
+++ src/program/Bank.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 
 public class Bank {
-    private String bankName; //
+    private String bankName;
     private List<Account> accounts;
     private List<Customer> customers;
 
@@ -13,6 +13,23 @@ public class Bank {
         this.accounts = new ArrayList<>();
         this.customers = new ArrayList<>();
     }
+
+    public void registerCustomer(Customer customer) {
+        customers.add(customer);
+    }
+
+    public void checkAccountByCustomerName(Customer customer) {
+        for(Account account : accounts) {
+            if(customer.getName().equals(account.getOwnerName())) {
+                String bankName = account.getBankName();
+                String accountNum = account.getAccountNumber();
+                int balance = account.getBalance();
+                System.out.printf("은행 : %s, 계좌번호 : %s, 잔고 : %d원",bankName,accountNum,balance);
+            }
+        }
+    }
+
+
 
 
 }
