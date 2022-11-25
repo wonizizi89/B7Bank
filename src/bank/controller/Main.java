cdd6fda2a5fa7d78339512594530345996e11bb2
beom <baek940dog@gmail.com>
2022-11-25 10:01:37 +0900

    revert

--- src/bank/controller/Main.java
+++
@@ -1,9 +0,0 @@
-package B7Bank;
-
-import java.util.Scanner;
-
-public class Main {
-   Scanner sc = new Scanner(System.in);
-
-
-}
--- src/bank/view/Main.java
+++
@@ -1,7 +0,0 @@
-package bank.view;
-
-public class Main {
-    public static void main (String[] args) {
-
-    }
-}
