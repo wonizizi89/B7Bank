b7f52d35a8ac3a68621110877f6c0f51e255ded7
Lee Sol <leeso4567@naver.com>
2022-11-26 21:42:29 +0900

    매개변수를 3개 받는 생성자 구현

--- src/bank/controller/Customer.java
+++ src/bank/controller/Customer.java
@@ -7,11 +7,19 @@ public class Customer {
     private String name;
     private List<Account> customerAccounts;
     private String customerId; // 동명 이인일 수 있어서 이걸로 식별
+    private String customerPassword;
 
     public Customer(String name, String customerId) {
         this.name = name;
         this.customerAccounts = new ArrayList<>();
         this.customerId = customerId;
+    }
+
+    public Customer(String name, String customerId, String customerPassword) {
+        this.name = name;
+        this.customerAccounts = new ArrayList<>();
+        this.customerId = customerId;
+        this.customerPassword = customerPassword;
     }
 
     public String getName() {
@@ -24,6 +32,10 @@ public class Customer {
 
     public String getCustomerId() {
         return customerId;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
     }
 
 }
